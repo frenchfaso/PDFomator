64844e0b099dcebf22bc61f0a95969b6b8977bea
Frenchfaso <frenchfaso@gmail.com>
2025-07-26 22:16:11 +0200

    Update service worker cache version to v1.0.5

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // PDFomator Service Worker
 // Simple offline cache for static assets
 
-const CACHE_NAME = 'pdfomator-v1.0.4';
+const CACHE_NAME = 'pdfomator-v1.0.5';
 const STATIC_ASSETS = [
     './',
     './index.html',
